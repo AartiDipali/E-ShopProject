c1cdf225153b6fa6423f74f7ced2150d263d3df3
AartiDipali <aartichame1217@gmail.com>
2022-09-29 14:48:12 +0530

    Added customer Home page

--- E-CommerceWebApplication/Areas/Customer/Controllers/HomeController.cs
+++ E-CommerceWebApplication/Areas/Customer/Controllers/HomeController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_CommerceWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace E_CommerceWebApplication.Areas.Customer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
